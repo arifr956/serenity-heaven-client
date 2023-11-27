c39ac169ac13b175c036c702483d0428778bb2e3
Arifur Rahman <arifr956@gmail.com>
2023-11-27 18:44:56 +0600

    dashboard created

--- src/Components/Home/Location.jsx
+++ src/Components/Home/Location.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+
 import { motion, AnimatePresence } from 'framer-motion';
+import SectionTitle from './SectionTitle';
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -30,7 +31,10 @@ const YourComponent = () => {
         variants={container}
       >
         <motion.div variants={item}>
-          <div className="bg-gray-200 w-full md:w-1/2 rounded-lg p-6 flex flex-col justify-between my-3">
+         <div>
+      <SectionTitle heading="Apartment Location" />
+      <div className="md:flex items-stretch ">
+        <div className="bg-gray-200 w-full md:w-1/2 rounded-lg p-6 flex flex-col justify-between my-3">
           <div className="card__content mt-4">
             <p className="text-gray-700">
               Discover the prime location of our apartment, situated in the heart
@@ -47,10 +51,8 @@ const YourComponent = () => {
             </div>
           </div>
         </div>
-        </motion.div>
 
-        <motion.div variants={item}>
-            <div className="w-full md:w-1/2 md:ml-4 my-3 ">
+        <div className="w-full md:w-1/2 md:ml-4 my-3 ">
           <iframe
             title="Apartment Location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.1659893651193!2d90.4085052!3d23.7913323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c70bec2ed6cd%3A0xd76cc5e621d2bb55!2sArsalan%20Zaman%2C%20Rd%20No.%2013E%2C%20Dhaka%201213!5e0!3m2!1sen!2sbd!4v1637946018762!5m2!1sen!2sbd"
@@ -62,9 +64,9 @@ const YourComponent = () => {
             className="w-full h-full rounded-lg"
           ></iframe>
         </div>
+      </div>
+    </div>
         </motion.div>
-
-        {/* Add more motion.div for additional items */}
       </motion.div>
     </AnimatePresence>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AuthProvider from './providers/AuthProvider';
 import Login from './Components/User/Login';
 import Registration from './Components/User/Registration/Registration';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 const router = createBrowserRouter([
   {
@@ -35,6 +36,15 @@ const router = createBrowserRouter([
       {
         path: "registration",
         element: <Registration></Registration>
+      }
+    ]
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard></Dashboard>,
+    children: [
+      {
+        
       }
     ]
   }
