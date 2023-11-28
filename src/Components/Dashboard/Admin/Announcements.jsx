bc4c838dff1eb4dd9d741a2d3b18badbe79baa62
Arifur Rahman <arifr956@gmail.com>
2023-11-28 14:02:34 +0600

    announcement form created with MAterial UI

--- src/Components/Dashboard/Admin/Announcements.jsx
+++ src/Components/Dashboard/Admin/Announcements.jsx
@@ -1,10 +1,95 @@
+import { FormControl, InputLabel, Input, Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import SectionTitle from '../../Home/SectionTitle';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { useState } from 'react';
 
-const Announcements = () => {
+const useStyles = makeStyles(() => ({
+    form: {
+        backgroundColor: '#f0f0f0',
+        margin: 2,
+    },
+    input: {
+        backgroundColor: 'white',
+        margin: 2,
+    },
+}));
+
+const MyForm = () => {
+    const classes = useStyles();
+    const axiosSecure = useAxiosSecure();
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+    });
+
+    const handleChange = (e) => {
+        // Update formData state based on input changes
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+
+        axiosSecure.post('/announcements', formData)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: "top-center",
+                        icon: "success",
+                        title: `Announcement Published`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            });
+    };
+
     return (
-        <div>
-            <h3>announcements</h3>
-        </div>
+        <>
+            <SectionTitle heading="Make announcement"></SectionTitle>
+            <form className={classes.form}>
+                <FormControl fullWidth>
+                    <InputLabel htmlFor="title">Title</InputLabel>
+                    <Input
+                        id="title"
+                        className={classes.input}
+                        aria-describedby="title-helper-text"
+                        value={formData.title}
+                        onChange={handleChange}
+                    />
+                </FormControl>
+
+                <FormControl fullWidth>
+                    <InputLabel htmlFor="description">Description</InputLabel>
+                    <Input
+                        id="description"
+                        className={classes.input}
+                        multiline
+                        rows={4}
+                        aria-describedby="description-helper-text"
+                        value={formData.description}
+                        onChange={handleChange}
+                    />
+                </FormControl>
+
+                <Button
+                    onClick={handleSubmit}
+                    variant="contained"
+                    style={{ backgroundColor: '#ff2c16' }}
+                    type="submit"
+                >
+                    Submit
+                </Button>
+            </form>
+        </>
     );
 };
 
-export default Announcements;
+export default MyForm;
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -7,7 +7,8 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation();
 
     if (loading) {
-        return <div className="animate-pulse flex flex-col items-center gap-4 w-60">
+        return <div className="flex justify-center align-middle items-center">
+            <div className="animate-pulse flex flex-col items-center gap-4 w-60">
             <div>
                 <div className="w-48 h-6 bg-slate-400 rounded-md"></div>
                 <div className="w-28 h-4 bg-slate-400 mx-auto mt-3 rounded-md"></div>
@@ -16,6 +17,7 @@ const PrivateRoute = ({ children }) => {
             <div className="h-7 bg-slate-400 w-full rounded-md"></div>
             <div className="h-7 bg-slate-400 w-full rounded-md"></div>
             <div className="h-7 bg-slate-400 w-1/2 rounded-md"></div>
+        </div>
         </div>
     }
 
